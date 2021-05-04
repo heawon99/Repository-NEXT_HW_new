cef80e0662638c3c2a3960ca71f0b742712dda11
heewon7755 <luby8899@korea.ac.kr>
2021-05-04 23:23:47 +0900

    크롤링 실습 1

--- session04/notebook.py
+++ session04/notebook.py
@@ -8,4 +8,27 @@ notebook_html = requests.get(NOTEBOOK_URL)
 # bs4 라이브러리를 통해 불러온 html을 우리가 원하는 형태로 파싱
 notebook_soup = BeautifulSoup(notebook_html.text,"html.parser")
 
-print(notebook_soup)
+notebook_list_box = notebook_soup.find("ul",{"class":"list_basis"})
+notebook_list=notebook_list_box.find_all("li",{"class":"basicList_item__2XT81"})
+title= notebook_list[3].find("div",{"class":"basicList_title__3P9Q7"}).find("a").string
+price=notebook_list[0].find("div",{"class":"basicList_price_area__1UXXR"}).find("span",{"class":"price_num__2WUXn"}).text
+
+
+
+#string을 붙인이유는 a태그 전체가 출력이 된다.string은 
+#우리가 텍스트만 출력하고 싶었기에 붙여진것 그래서 .text로 해도 괜찮음.
+
+result = {
+    'title':title,
+    'price':price
+}
+print(result)
+
+# print(len(notebook_items))
+# print(type(notebook_items))
+
+#find 매칭된 첫번째 태그만 가져오는거 
+#find all 모든 태그를 가져오는거 
+#notebook_list_box items 5개를 가지고 있는 부모 요소 
+#notebook_items 5개 있음. 5개의 배열을 의미함. 
+
