500470efd7dcfcc42d4ce2a6adc0870ca19f3788
heewon7755 <luby8899@korea.ac.kr>
2021-05-05 23:15:33 +0900

    session06 과제

---
+++ session06/blogProjectpractice/blogApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.CharField(max_length=20)
+    time = models.TextField(default='')
+
+    def __str__(self):
+        return self.title
